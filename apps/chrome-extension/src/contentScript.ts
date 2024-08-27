9bb93ffc60b7a96744c3e19834125e5a1bef565d
edwardoranj <175904393+edwardoranj@users.noreply.github.com>
2024-08-27 09:35:49 +0900

    update extension

--- apps/chrome-extension/src/contentScript.ts
+++ apps/chrome-extension/src/contentScript.ts
@@ -1,30 +1,59 @@
-import '@dialectlabs/blinks/index.css';
-import { setupTwitterObserver } from '@dialectlabs/blinks/ext/twitter';
-import { ActionConfig } from '@dialectlabs/blinks';
+import '@blinks-icp/core/index.css';
+import { ActionConfig, setupTwitterObserver } from '@blinks-icp/core';
+import { createClient, defaultProviders } from '@blinks-icp/wallet-adapter';
+import type { IDL } from '@dfinity/candid';
 
-const adapter = (wallet: string) =>
-  new ActionConfig(import.meta.env.VITE_RPC_URL, {
-    signTransaction: (tx: string) =>
-      chrome.runtime.sendMessage({
-        type: 'sign_transaction',
-        wallet,
-        payload: {
-          txData: tx,
-        },
-      }),
-    connect: () =>
-      chrome.runtime.sendMessage({
-        wallet,
-        type: 'connect',
-      }),
+export const host =
+  import.meta.env.VITE_DFX_NETWORK === 'ic'
+    ? 'https://icp0.io'
+    : 'http://127.0.0.1:4943';
+
+export const provider =
+  import.meta.env.VITE_DFX_NETWORK === 'ic'
+    ? 'https://identity.ic0.app'
+    : 'http://rdmx6-jaaaa-aaaaa-aaadq-cai.localhost:4943';
+
+let count = 1;
+
+const adapter = (wallet: string) => {
+  const client = createClient({
+    providers: defaultProviders({
+      host,
+      providerUrl: provider,
+    }),
+    globalProviderConfig: {
+      host,
+    },
   });
+
+  return new ActionConfig(host, {
+    createActor: async (
+      canisterId: string,
+      idlFactory: IDL.InterfaceFactory,
+    ) => {
+      const actorResult = await client.activeProvider?.createActor(
+        canisterId,
+        idlFactory,
+      );
+
+      if (!actorResult || actorResult.isErr()) {
+        return { error: 'Unable to create actor' };
+      }
+      const actor = actorResult.value;
+      return { actor };
+    },
+    connect: async () => {
+      if (client.status === 'connected') {
+        return client.activeProvider!.principal!;
+      }
+      const { activeProvider } = await client.connectAsync('ii');
+      return activeProvider.principal ?? null;
+    },
+  });
+};
 
 function initTwitterObserver() {
-  chrome.runtime.sendMessage({ type: 'getSelectedWallet' }, (wallet) => {
-    if (wallet) {
-      setupTwitterObserver(adapter(wallet));
-    }
-  });
+  setupTwitterObserver(adapter('ii'));
 }
 
 initTwitterObserver();
