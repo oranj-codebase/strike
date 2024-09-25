941817df9c1443d176040494d5b18424e020d43c
devworker92422 <devworker92422@gmail.com>
2024-09-25 21:36:31 +0900

    add href to footer icon

--- apps/site/src/views/home/index.tsx
+++ apps/site/src/views/home/index.tsx
@@ -311,12 +311,12 @@ export function Home() {
           </div>
         </section>
         <footer className="flex flex-row justify-between py-[48px]">
-          <div className="flex items-center gap-[4.23px]">
+          <a className="flex items-center gap-[4.23px]" href="/">
             <StrikeLogo width={24} height={24} />
             <span className="font-bold font-syne text-[19.64px] leading-[19.64px]">
               STRIKE
             </span>
-          </div>
+          </a>
           <div className="flex flex-row gap-[32px] text-[14px] leading-[24px] font-medium">
             <a href="/privacy">Privacy Policy</a>
             <a href="/">Visit Oranj</a>
